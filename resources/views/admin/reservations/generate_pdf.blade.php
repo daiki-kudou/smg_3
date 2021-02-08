da4e8fa869cead7b56e5f2337e067f1b608e2e7a
maru-star <maruoka@web-trickster.com>
2021-02-08 15:10:01 +0900

    pdf rechange

--- resources/views/admin/reservations/generate_pdf.blade.php
+++ resources/views/admin/reservations/generate_pdf.blade.php
@@ -22,19 +22,111 @@
     body {
       font-family: migmix-1p-regular;
     }
+
+    p {
+      margin: 0;
+      padding: 0;
+    }
+
+    /* board css------------------------------------------------------- */
+    .board-box {
+      /* width: 100%; */
+      /* /* width: 297px; */
+      /* height: 210px; */
+      /* margin: 50px auto; */
+      padding: 30px;
+      /* border: 1px solid #eee; */
+      /* box-shadow: 0 0 10px rgba(0, 0, 0, .15); */
+      font-size: 15px;
+      /* line-height: 24px; */
+      color: #333;
+      border: 5px solid #B08046;
+      /* background: no-repeat center/98% url(data:image/png;base64,); */
+    }
+
+    .board-box .board-inner {
+      width: 95%;
+      margin: 0 auto;
+      /* line-height: inherit; */
+      text-align: left;
+    }
+
+    .board-box .date td {
+      font-size: 30px;
+      padding-bottom: 2%;
+    }
+
+    .board-box .event-name td,
+    .board-box .event-name2 td {
+      font-size: 60px;
+      /* padding-bottom: 30px; */
+    }
+
+    .board-box .event-owner td {
+      font-size: 30px;
+      padding-top: 30px;
+      padding-bottom: 15%;
+    }
+
+    .board-box .venue td {
+      font-size: 50px;
+      text-align: right;
+      padding-top: 2%;
+      border-top: 2px solid #ddd;
+    }
   </style>
+
 </head>
 
 <body>
-  <div class="wrapper">
+
+  <!-- <div class="wrapper">
     <h1>{{$reservation->reserve_date}}</h1>
     <h1>{{$reservation->event_start}}~{{$reservation->event_finish}}</h1>
     <h1>{{$reservation->event_name1}}</h1>
     <h1>{{$reservation->event_name2}}</h1>
     <h1>主催：{{$reservation->event_owner}}</h1>
     <h1>{{$reservation->venue->name_area}}{{$reservation->venue->name_bldg}}{{$reservation->venue->name_venue}}</h1>
+  </div>  -->
 
+
+  <div class="board-box">
+    <table cellpadding="0" cellspacing="0" class="board-inner">
+      <tr class="date">
+        <td>
+          <p>{{$reservation->reserve_date}}</p>
+          <p>{{$reservation->event_start}}<span>~</span>{{$reservation->event_finish}}</p>
+        </td>
+      </tr>
+      <tr class="event-name">
+        <td>
+          <p>イベントの名前は16文字までです</p>
+          <!-- <p>{{$reservation->event_name1}}</p> -->
+        </td>
+      </tr>
+      <tr class="event-name2">
+        <td>
+          <p>イベントの名前は16文字までです</p>
+          <!-- <p>{{$reservation->event_name2}}</p> -->
+        </td>
+      </tr>
+
+      <tr class="event-owner">
+        <td>
+          <p>主催：ここの主催者の名前は、30文字以内です。あままままままままま</p>
+          <!-- <p><span>主催：</span>{{$reservation->event_owner}}</p> -->
+        </td>
+      </tr>
+
+      <tr class="venue">
+        <td>
+          <p>サンワールドビル2号室</p>
+          <!-- <p>{{$reservation->venue->name_area}}{{$reservation->venue->name_bldg}}{{$reservation->venue->name_venue}}</p> -->
+        </td>
+      </tr>
+    </table>
   </div>
+
 </body>
 
 </html>
