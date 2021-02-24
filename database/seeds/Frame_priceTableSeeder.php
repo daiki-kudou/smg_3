16c7bd6ba0e8af563d574fff3f6cf69933d7308a
daiki-kudou <67718513+daiki-kudou@users.noreply.github.com>
2021-02-24 20:38:15 +0900

    fix again

--- database/seeds/Frame_priceTableSeeder.php
+++ database/seeds/Frame_priceTableSeeder.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Database\Seeder;
 
+
 use App\Models\Venue;
+use App\Models\Frame_price;
+
 
 class Frame_priceTableSeeder extends Seeder
 {
@@ -13,49 +16,53 @@ class Frame_priceTableSeeder extends Seeder
    */
   public function run()
   {
-    $venue = Venue::find(1);
-    $venue->frame_prices()->create([
-      // 'venue_id' => 1,
+    Frame_price::create([
+      'venue_id' => 1,
       'frame' => '午前',
       'start' => '10:00:00',
       'finish' => '12:00:00',
       'price' => 15000,
       'extend' => 5000,
     ]);
-    $venue->frame_prices()->create([
-      // 'venue_id' => $venue->id,
+
+    Frame_price::create([
+      'venue_id' => 1,
       'frame' => '午後',
       'start' => '13:00:00',
       'finish' => '17:00:00',
       'price' => 30000,
       'extend' => 5000,
     ]);
-    $venue->frame_prices()->create([
-      // 'venue_id' => $venue->id,
+
+    Frame_price::create([
+      'venue_id' => 1,
       'frame' => '夜間',
       'start' => '18:00:00',
       'finish' => '23:00:00',
       'price' => 15000,
       'extend' => 5000,
     ]);
-    $venue->frame_prices()->create([
-      // 'venue_id' => $venue->id,
+
+    Frame_price::create([
+      'venue_id' => 1,
       'frame' => '午前＆午後',
       'start' => '10:00:00',
       'finish' => '17:00:00',
       'price' => 36000,
       'extend' => 5000,
     ]);
-    $venue->frame_prices()->create([
-      // 'venue_id' => $venue->id,
+
+    Frame_price::create([
+      'venue_id' => 1,
       'frame' => '午後＆夜間',
       'start' => '13:00:00',
       'finish' => '21:00:00',
       'price' => 36000,
       'extend' => 5000,
     ]);
-    $venue->frame_prices()->create([
-      // 'venue_id' => $venue->id,
+
+    Frame_price::create([
+      'venue_id' => 1,
       'frame' => '終日',
       'start' => '10:00:00',
       'finish' => '21:00:00',
@@ -66,102 +73,117 @@ class Frame_priceTableSeeder extends Seeder
 
 
 
-    // $venue2 = Venue::find(2);
-    // $venue2->frame_prices()->create([
-    //   // 'venue_id' => 2,
-    //   'frame' => '午前',
-    //   'start' => '10:00:00',
-    //   'finish' => '12:00:00',
-    //   'price' => 17000,
-    //   'extend' => 6000,
-    // ]);
-    // $venue2->frame_prices()->create([
-    //   // 'venue_id' => $venue2->id,
-    //   'frame' => '午後',
-    //   'start' => '13:00:00',
-    //   'finish' => '17:00:00',
-    //   'price' => 36000,
-    //   'extend' => 6000,
-    // ]);
-    // $venue2->frame_prices()->create([
-    //   // 'venue_id' => $venue2->id,
-    //   'frame' => '夜間',
-    //   'start' => '18:00:00',
-    //   'finish' => '23:00:00',
-    //   'price' => 17000,
-    //   'extend' => 6000,
-    // ]);
-    // $venue2->frame_prices()->create([
-    //   // 'venue_id' => $venue2->id,
-    //   'frame' => '午前＆午後',
-    //   'start' => '10:00:00',
-    //   'finish' => '17:00:00',
-    //   'price' => 42000,
-    //   'extend' => 6000,
-    // ]);
-    // $venue2->frame_prices()->create([
-    //   // 'venue_id' => $venue2->id,
-    //   'frame' => '午後＆夜間',
-    //   'start' => '13:00:00',
-    //   'finish' => '21:00:00',
-    //   'price' => 42000,
-    //   'extend' => 6000,
-    // ]);
-    // $venue2->frame_prices()->create([
-    //   // 'venue_id' => $venue2->id,
-    //   'frame' => '終日',
-    //   'start' => '10:00:00',
-    //   'finish' => '21:00:00',
-    //   'price' => 50000,
-    //   'extend' => 6000,
-    // ]);
 
 
 
-
-    $venue3 = Venue::find(3);
-    $venue3->frame_prices()->create([
-      // 'venue_id' => 3,
+    Frame_price::create([
+      'venue_id' => 2,
       'frame' => '午前',
       'start' => '10:00:00',
       'finish' => '12:00:00',
       'price' => 17000,
       'extend' => 6000,
     ]);
-    $venue3->frame_prices()->create([
-      // 'venue_id' => $venue3->id,
+
+    Frame_price::create([
+      'venue_id' => 2,
       'frame' => '午後',
       'start' => '13:00:00',
       'finish' => '17:00:00',
       'price' => 36000,
       'extend' => 6000,
     ]);
-    $venue3->frame_prices()->create([
-      // 'venue_id' => $venue3->id,
+
+    Frame_price::create([
+      'venue_id' => 2,
       'frame' => '夜間',
       'start' => '18:00:00',
       'finish' => '23:00:00',
       'price' => 17000,
       'extend' => 6000,
     ]);
-    $venue3->frame_prices()->create([
-      // 'venue_id' => $venue3->id,
+
+    Frame_price::create([
+      'venue_id' => 2,
       'frame' => '午前＆午後',
       'start' => '10:00:00',
       'finish' => '17:00:00',
       'price' => 42000,
       'extend' => 6000,
     ]);
-    $venue3->frame_prices()->create([
-      // 'venue_id' => $venue3->id,
+
+    Frame_price::create([
+      'venue_id' => 2,
       'frame' => '午後＆夜間',
       'start' => '13:00:00',
       'finish' => '21:00:00',
       'price' => 42000,
       'extend' => 6000,
     ]);
-    $venue3->frame_prices()->create([
-      // 'venue_id' => $venue3->id,
+
+    Frame_price::create([
+      'venue_id' => 2,
+      'frame' => '終日',
+      'start' => '10:00:00',
+      'finish' => '21:00:00',
+      'price' => 50000,
+      'extend' => 6000,
+    ]);
+
+
+
+
+
+
+
+
+    Frame_price::create([
+      'venue_id' => 3,
+      'frame' => '午前',
+      'start' => '10:00:00',
+      'finish' => '12:00:00',
+      'price' => 17000,
+      'extend' => 6000,
+    ]);
+
+    Frame_price::create([
+      'venue_id' => 3,
+      'frame' => '午後',
+      'start' => '13:00:00',
+      'finish' => '17:00:00',
+      'price' => 36000,
+      'extend' => 6000,
+    ]);
+
+    Frame_price::create([
+      'venue_id' => 3,
+      'frame' => '夜間',
+      'start' => '18:00:00',
+      'finish' => '23:00:00',
+      'price' => 17000,
+      'extend' => 6000,
+    ]);
+
+    Frame_price::create([
+      'venue_id' => 3,
+      'frame' => '午前＆午後',
+      'start' => '10:00:00',
+      'finish' => '17:00:00',
+      'price' => 42000,
+      'extend' => 6000,
+    ]);
+
+    Frame_price::create([
+      'venue_id' => 3,
+      'frame' => '午後＆夜間',
+      'start' => '13:00:00',
+      'finish' => '21:00:00',
+      'price' => 42000,
+      'extend' => 6000,
+    ]);
+
+    Frame_price::create([
+      'venue_id' => 3,
       'frame' => '終日',
       'start' => '10:00:00',
       'finish' => '21:00:00',
