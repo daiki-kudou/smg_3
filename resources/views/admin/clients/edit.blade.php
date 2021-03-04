3562c3cd4885dcc8daed0b91b7c7e63b9a435785
maru-star <maruoka@web-trickster.com>
2021-03-04 12:22:05 +0000

    clients

--- resources/views/admin/clients/edit.blade.php
+++ resources/views/admin/clients/edit.blade.php
@@ -1,24 +1,46 @@
 @extends('layouts.admin.app')
-
 @section('content')
-<div class="container">
-  <h2>顧客管理　編集</h2>
+<script src="{{ asset('/js/template.js') }}"></script>
+<script src="{{ asset('/js/admin/validation.js') }}"></script>
+<script src="{{ asset('/js/admin/reservation.js') }}"></script>
+<link href="{{ asset('/css/template.css') }}" rel="stylesheet">
+
+<div class="container-field">
+  <div class="float-right">
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item active">
+          ダミーダミーダミーダミー
+        </li>
+      </ol>
+    </nav>
+  </div>
+  <h2 class="mt-3 mb-3">顧客管理 編集</h2>
+  <hr>
+</div>
+
+<section class="section-wrap">
   <div class="row">
-    <div class="col-sm">
+    <!-- 左側の項目 ---------------------------------------------------------->
+    <div class="col">
       {{Form::model($user, ['route' => ['admin.clients.update', $user->id], 'method' => 'put'])}}
-      <table class="table">
+      <table class="table table-bordered">
         <thead>
           <tr>
-            <th scope="col"><i class="fas fa-exclamation-circle fa-fw"></i></i>基本情報</th>
+            <td colspan="2">
+              <p class="title-icon">
+                <i class="fas fa-exclamation-circle icon-size fa-fw"></i>基本情報
+              </p>
+            </td>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <th scope="row">{{ Form::label('company', '会社・団体名') }}</th>
+            <th class="table-active">{{ Form::label('company', '会社・団体名') }}</th>
             <td>{{ Form::text('company', $user->company, ['class' => 'form-control']) }}</td>
           </tr>
           <tr>
-            <th scope="row">{{ Form::label('post_code', '郵便番号') }}</th>
+            <th class="table-active">{{ Form::label('post_code', '郵便番号') }}</th>
             <td>{{ Form::text('post_code', $user->post_code, [
                             'class' => 'form-control',
                             'onKeyUp'=>"AjaxZip3.zip2addr(this,'','address1','address2');",
@@ -27,151 +49,166 @@
             </td>
           </tr>
           <tr>
-            <th scope="row">{{ Form::label('address1', '住所1（都道府県）') }}</th>
+            <th class="table-active">{{ Form::label('address1', '住所1（都道府県）') }}</th>
             <td>{{ Form::text('address1', $user->address1, ['class' => 'form-control']) }}</td>
           </tr>
           <tr>
-            <th scope="row">{{ Form::label('address2', '住所2（市町村番地）') }}</th>
+            <th class="table-active">{{ Form::label('address2', '住所2（市町村番地）') }}</th>
             <td>{{ Form::text('address2', $user->address2, ['class' => 'form-control']) }}</td>
           </tr>
           <tr>
-            <th scope="row">{{ Form::label('address3', '住所3（建物名）') }}</th>
+            <th class="table-active">{{ Form::label('address3', '住所3（建物名）') }}</th>
             <td>{{ Form::text('address3', $user->address3, ['class' => 'form-control']) }}</td>
           </tr>
           <tr>
-            <th scope="row">{{ Form::label('address_remark', '住所備考') }}</th>
+            <th class="table-active">{{ Form::label('address_remark', '住所備考') }}</th>
             <td>{{ Form::textarea('address_remark', $user->address_remark, ['class' => 'form-control']) }}
             </td>
           </tr>
           <tr>
-            <th scope="row">{{ Form::label('url', '会社・団体名URL') }}</th>
+            <th class="table-active">{{ Form::label('url', '会社・団体名URL') }}</th>
             <td>{{ Form::text('url', $user->url, ['class' => 'form-control']) }}</td>
           </tr>
           <tr>
-            <th scope="row">{{ Form::label('attr', '顧客属性') }}</th>
-            <td>{{Form::select('attr', [1=>'一般企業', 2=>'上場企業',3=>'近隣利用', 4=>'講師・セミナー', 5=>'その他'],$user->attr)}}
+            <th class="table-active">{{ Form::label('attr', '顧客属性') }}</th>
+            <td>{{Form::select('attr', [1=>'一般企業', 2=>'上場企業',3=>'近隣利用', 4=>'講師・セミナー', 5=>'ネットワーク', 6=>'その他'],$user->attr)}}
             </td>
           </tr>
           <tr>
-            <th scope="row">{{ Form::label('condition', '割引条件') }}</th>
+            <th class="table-active">{{ Form::label('condition', '割引条件') }}</th>
             <td>{{ Form::text('condition', old('condition'), ['class' => 'form-control']) }}</td>
           </tr>
         </tbody>
       </table>
     </div>
-    <div class="col-sm">
-      <table class="table">
+    <!-- 左側の項目 終わり---------------------------------------------------------->
+
+    <!-- 右側の項目 ---------------------------------------------------------->
+    <div class="col">
+      <table class="table table-bordered">
         <thead>
           <tr>
-            <th scope="col"><i class="fas fa-user fa-fw"></i>担当者情報</th>
+            <p class="title-icon">
+              <td colspan="3"><i class="fas fa-user fa-fw icon-size"></i>担当者情報
+            </p>
+            </td>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <th scope="row">{{ Form::label('first_name', '担当者氏名') }}</th>
+            <th class="table-active">{{ Form::label('first_name', '担当者氏名') }}</th>
             <td>姓：{{ Form::text('first_name', $user->first_name, ['class' => 'form-control']) }}
             </td>
             <td>名：{{ Form::text('last_name', $user->last_name, ['class' => 'form-control']) }}
             </td>
           </tr>
           <tr>
-            <th scope="row">{{ Form::label('first_name_kana', '担当者氏名（ふりがな）') }}</th>
+            <th class="table-active">{{ Form::label('first_name_kana', '担当者氏名（フリガナ）') }}</th>
             <td>セイ：{{ Form::text('first_name_kana', $user->first_name_kana, ['class' => 'form-control'])}}
             </td>
             <td>メイ：{{ Form::text('last_name_kana', $user->last_name_kana, ['class' => 'form-control']) }}
             </td>
           </tr>
           <tr>
-            <th scope="row">{{ Form::label('tel', '電話番号') }}</th>
-            <td>{{ Form::text('tel', $user->tel, ['class' => 'form-control']) }}
+            <th class="table-active">{{ Form::label('tel', '電話番号') }}</th>
+            <td colspan="2">{{ Form::text('tel', $user->tel, ['class' => 'form-control']) }}
             </td>
           </tr>
           <tr>
-            <th scope="row">{{ Form::label('mobile', '携帯番号') }}</th>
-            <td>{{ Form::text('mobile', $user->mobile, ['class' => 'form-control']) }}
+            <th class="table-active">{{ Form::label('mobile', '携帯番号') }}</th>
+            <td colspan="2">{{ Form::text('mobile', $user->mobile, ['class' => 'form-control']) }}
             </td>
           </tr>
           <tr>
-            <th scope="row">{{ Form::label('email', '担当者メールアドレス') }}</th>
-            <td>{{ Form::text('email', $user->email, ['class' => 'form-control']) }}</td>
+            <th class="table-active">{{ Form::label('email', '担当者メールアドレス') }}</th>
+            <td colspan="2">{{ Form::text('email', $user->email, ['class' => 'form-control']) }}</td>
           </tr>
           <tr>
-            <th scope="row">{{ Form::label('fax', 'FAX') }}</th>
-            <td>{{ Form::text('fax', $user->fax, ['class' => 'form-control']) }}</td>
+            <th class="table-active">{{ Form::label('fax', 'FAX') }}</th>
+            <td colspan="2">{{ Form::text('fax', $user->fax, ['class' => 'form-control']) }}</td>
           </tr>
-          <thead>
-            <tr>
-              <th scope="col"><i class="fas fa-user fa-fw"></i>支払いデータ</th>
-            </tr>
-            <tr>
-              <th scope="row">{{ Form::label('pay_method', '支払方法') }}</th>
-              <td>{{Form::select('pay_method', [1=>'銀行振込', 2=>'現金',3=>'クレジットカード', 4=>'スマホ決済'],$user->pay_method)}}
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">{{ Form::label('pay_limit', '支払期日') }}</th>
-              <td>{{Form::select('pay_limit', [1=>'3営業日前', 2=>'当月末',3=>'翌月末'],$user->pay_limit)}}</td>
-            </tr>
-            <tr>
-              <th scope="row">{{ Form::label('pay_post_code', '請求書送付先郵便番号') }}</th>
-              <td>{{ Form::text('pay_post_code', $user->pay_post_code, [
+        <tbody>
+      </table>
+
+      <!-- 支払いデータ ------------------------------------------------------------>
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <td colspan="3">
+              <p class="title-icon">
+                <i class="fas fa-yen-sign icon-size"></i>支払いデータ
+              </p>
+            </td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th class="table-active">{{ Form::label('pay_method', '支払方法') }}</th>
+            <td>{{Form::select('pay_method', [1=>'銀行振込', 2=>'現金',3=>'クレジットカード', 4=>'スマホ決済'],$user->pay_method)}}
+            </td>
+          </tr>
+          <tr>
+            <th class="table-active">{{ Form::label('pay_limit', '支払期日') }}</th>
+            <td>{{Form::select('pay_limit', [1=>'当月末〆当月末CASH', 2=>'当月末〆翌月末CASH',3=>'当月末〆翌々月末CACH',4=>'当月末〆3カ月末CASH'],$user->pay_limit)}}</td>
+          </tr>
+          <tr>
+            <th class="table-active">{{ Form::label('pay_post_code', '請求書送付先郵便番号') }}</th>
+            <td>{{ Form::text('pay_post_code', $user->pay_post_code, [
                                 'class' => 'form-control pay_post_code',
                                 'onKeyUp'=>"AjaxZip3.zip2addr(this,'','pay_address1','pay_address2');",
                                 'autocomplete'=>'off',
                                 ]) }}
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">{{ Form::label('pay_address1', '請求書送付先（都道府県）') }}</th>
-              <td>{{ Form::text('pay_address1', $user->pay_address1, ['class' => 'form-control pay_address_1']) }}
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">{{ Form::label('pay_address2', '請求書送付先（市町村番地）') }}</th>
-              <td>{{ Form::text('pay_address2',$user->pay_address2, ['class' => 'form-control pay_address_2']) }}
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">{{ Form::label('pay_address3', '建物名') }}</th>
-              <td>{{ Form::text('pay_address3',$user->pay_address3, ['class' => 'form-control']) }}
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">{{ Form::label('pay_remark', '請求書備考') }}</th>
-              <td>{{ Form::textarea('pay_remark', $user->pay_remark, ['class' => 'form-control']) }}</td>
-            </tr>
-          </thead>
+            </td>
+          </tr>
+          <tr>
+            <th class="table-active">{{ Form::label('pay_address1', '請求書送付先（都道府県）') }}</th>
+            <td>{{ Form::text('pay_address1', $user->pay_address1, ['class' => 'form-control pay_address_1']) }}
+            </td>
+          </tr>
+          <tr>
+            <th class="table-active">{{ Form::label('pay_address2', '請求書送付先（市町村番地）') }}</th>
+            <td>{{ Form::text('pay_address2',$user->pay_address2, ['class' => 'form-control pay_address_2']) }}
+            </td>
+          </tr>
+          <tr>
+            <th class="table-active">{{ Form::label('pay_address3', '請求書送付先 (建物名)') }}</th>
+            <td>{{ Form::text('pay_address3',$user->pay_address3, ['class' => 'form-control']) }}
+            </td>
+          </tr>
+          <tr>
+            <th class="table-active">{{ Form::label('pay_remark', '請求書備考') }}</th>
+            <td>{{ Form::textarea('pay_remark', $user->pay_remark, ['class' => 'form-control']) }}</td>
+          </tr>
         </tbody>
       </table>
     </div>
+    <!-- 右側の項目 終わり---------------------------------------------------------->
+
   </div>
   <div class="row">
-    <div class="col-sm">
-      <table class="table">
+    <div class="col">
+      <table class="table table-bordered">
         <thead>
           <tr>
-            <th scope="col">注意事項</th>
+            <th class="table-active caution caution_bg">{{ Form::label('attention', '注意事項') }}</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <th scope="row">{{ Form::label('attention', '注意事項') }}</th>
-            <td>{{ Form::textarea('attention', $user->attention, ['class' => 'form-control']) }}</td>
+            <td class="caution">{{ Form::textarea('attention', $user->attention, ['class' => 'form-control']) }}</td>
           </tr>
-
         </tbody>
       </table>
     </div>
-    <div class="col-sm">
-      <table class="table">
+    <div class="col">
+      <table class="table table-bordered">
         <thead>
           <tr>
-            <th scope="col">備考</th>
+            <th class="table-active">{{ Form::label('remark', '備考') }}</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <th scope="row">{{ Form::label('remark', '備考') }}</th>
             <td>{{ Form::textarea('remark', $user->remark, ['class' => 'form-control']) }}</td>
           </tr>
           </thead>
@@ -179,7 +216,13 @@
       </table>
     </div>
   </div>
-  {{ Form::submit('更新', ['class' => 'btn btn-primary btn-block']) }}
-  {{ Form::close() }}
-</div>
+
+  <div class="mt-5">
+    {{ Form::submit('保存する', ['class' => 'btn more_btn_lg d-block btn-lg mx-auto my-5']) }}
+    {{ Form::close() }}
+  </div>
+
+</section>
+
+
 @endsection
--- resources/views/admin/clients/show.blade.php
+++ resources/views/admin/clients/show.blade.php
@@ -1,63 +1,84 @@
 @extends('layouts.admin.app')
 
 @section('content')
-<div class="container">
-  <h2>顧客管理　詳細</h2>
+<link href="{{ asset('/css/template.css') }}" rel="stylesheet">
+
+<div class="container-field">
+  <div class="float-right">
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item active">
+        ダミーダミーダミーダミー
+        </li>
+      </ol>
+    </nav>
+  </div>
+  <h2 class="mt-3 mb-3">顧客管理 詳細</h2>
+  <hr>
+</div>
+
+<section class="section-wrap">
+  <div class="align-items-center d-flex justify-content-between mb-3">
   <div>
-    <div>
-      {{ link_to_route('admin.clients.edit', '編集する', $parameters = $user->id, ['class' => 'btn btn-primary']) }}
-    </div>
-    <div>
       {{ Form::model($user, ['route' => ['admin.clients.destroy', $user->id], 'method' => 'delete']) }}
       @csrf
-      {{ Form::submit('削除', ['class' => 'btn btn-danger']) }}
+      {{ Form::submit('削除', ['class' => 'btn more_btn4']) }}
       {{ Form::close() }}
     </div>
+    <div>
+      {{ link_to_route('admin.clients.edit', '編集する', $parameters = $user->id, ['class' => 'btn more_btn']) }}
+    </div>
   </div>
+
   <div class="row">
-    <div class="col-sm">
-      <table class="table">
+    <!-- 左側の項目 ---------------------------------------------------->
+    <div class="col">
+      <table class="table table-bordered">
         <thead>
           <tr>
-            <th scope="col"><i class="fas fa-exclamation-circle fa-fw"></i></i>基本情報</th>
+            <td colspan="2">
+              <p class="title-icon">
+                <i class="fas fa-exclamation-circle icon-size fa-fw"></i>基本情報
+              </p>
+            </td>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <th scope="row">{{ Form::label('id', '利用者ID') }}</th>
+            <th class="table-active">{{ Form::label('id', '利用者ID') }}</th>
             <td>{{$user->id}}</td>
           </tr>
           <tr>
-            <th scope="row">{{ Form::label('company', '会社・団体名') }}</th>
+            <th class="table-active">{{ Form::label('company', '会社・団体名') }}</th>
             <td>{{$user->company}}</td>
           </tr>
           <tr>
-            <th scope="row">{{ Form::label('post_code', '郵便番号') }}</th>
+            <th class="table-active">{{ Form::label('post_code', '郵便番号') }}</th>
             <td>{{$user->post_code}}</td>
           </tr>
           <tr>
-            <th scope="row">{{ Form::label('address1', '住所1（都道府県）') }}</th>
+            <th class="table-active">{{ Form::label('address1', '住所1（都道府県）') }}</th>
             <td>{{$user->address1}}</td>
           </tr>
           <tr>
-            <th scope="row">{{ Form::label('address2', '住所2（市町村番地）') }}</th>
+            <th class="table-active">{{ Form::label('address2', '住所2（市町村番地）') }}</th>
             <td>{{$user->address2}}</td>
           </tr>
           <tr>
-            <th scope="row">{{ Form::label('address3', '住所3（建物名）') }}</th>
+            <th class="table-active">{{ Form::label('address3', '住所3（建物名）') }}</th>
             <td>{{$user->address3}}</td>
             </td>
           </tr>
           <tr>
-            <th scope="row">{{ Form::label('address_remark', '住所備考') }}</th>
+            <th class="table-active">{{ Form::label('address_remark', '住所備考') }}</th>
             <td>{{$user->address_remark}}</td>
           </tr>
           <tr>
-            <th scope="row">{{ Form::label('url', '会社・団体名URL') }}</th>
+            <th class="table-active">{{ Form::label('url', '会社・団体名URL') }}</th>
             <td>{{$user->url}}</td>
           </tr>
           <tr>
-            <th scope="row">{{ Form::label('attr', '顧客属性') }}</th>
+            <th class="table-active">{{ Form::label('attr', '顧客属性') }}</th>
             <td>
               @if ($user->attr==1)
               一般企業
@@ -68,127 +89,157 @@
               @elseif($user->attr==4)
               講師・セミナー
               @elseif($user->attr==5)
+              ネットワーク
+              @elseif($user->attr==6)
               その他
               @endif
             </td>
           </tr>
           <tr>
-            <th scope="row">{{ Form::label('condition', '割引条件') }}</th>
+            <th class="table-active">{{ Form::label('condition', '割引条件') }}</th>
             <td>{{$user->condition}}</td>
           </tr>
         </tbody>
       </table>
     </div>
-    <div class="col-sm">
-      <table class="table">
+    <!-- 左側の項目 終わり---------------------------------------------------->
+
+
+    <!-- 右側の項目 ---------------------------------------------------->
+    <div class="col">
+      <!-- 担当者情報 ------------------------------------------------------>
+      <table class="table table-bordered">
         <thead>
           <tr>
-            <th scope="col"><i class="fas fa-user fa-fw"></i>担当者情報</th>
+            <p class="title-icon">
+              <td colspan="3"><i class="fas fa-user fa-fw icon-size"></i>担当者情報
+            </p>
+            </td>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <th scope="row">{{ Form::label('first_name', '担当者氏名') }}</th>
+            <th class="table-active">{{ Form::label('first_name', '担当者氏名') }}</th>
             <td>{{$user->first_name}}　{{$user->last_name}}</td>
           </tr>
           <tr>
-            <th scope="row">{{ Form::label('first_name_kana', '担当者氏名（ふりがな）') }}</th>
+            <th class="table-active">{{ Form::label('first_name_kana', '担当者氏名（フリガナ）') }}</th>
             <td>{{$user->first_name_kana}}{{$user->last_name_kana}}</td>
           </tr>
           <tr>
-            <th scope="row">{{ Form::label('tel', '電話番号') }}</th>
+            <th class="table-active">{{ Form::label('mobile', '携帯電話') }}</th>
+            <td>{{$user->mobile}}</td>
+          </tr>
+          <tr>
+            <th class="table-active">{{ Form::label('tel', '固定電話') }}</th>
             <td>{{$user->tel}}</td>
           </tr>
           <tr>
-            <th scope="row">{{ Form::label('email', '担当者メールアドレス') }}</th>
+            <th class="table-active">{{ Form::label('email', '担当者メールアドレス') }}</th>
             <td>{{$user->email}}</td>
           </tr>
           <tr>
-            <th scope="row">{{ Form::label('fax', 'FAX') }}</th>
+            <th class="table-active">{{ Form::label('fax', 'FAX') }}</th>
             <td>{{$user->fax}}</td>
           </tr>
-          <thead>
-            <tr>
-              <th scope="col"><i class="fas fa-user fa-fw"></i>支払いデータ</th>
-            </tr>
-            <tr>
-              <th scope="row">{{ Form::label('pay_method', '支払方法') }}</th>
-              <td>
-                @if ($user->pay_method==1)
-                銀行振込
-                @elseif($user->pay_method==2)
-                現金
-                @elseif($user->pay_method==3)
-                クレジットカード
-                @elseif($user->pay_method==4)
-                スマホ決済
-                @endif
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">{{ Form::label('pay_limit', '支払期日') }}</th>
-              <td>
-                @if ($user->pay_limit==1)
-                3営業日前
-                @elseif($user->pay_limit==2)
-                当月末
-                @elseif($user->pay_limit==3)
-                翌月末
-                @endif
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">{{ Form::label('pay_post_code', '請求書送付先郵便番号') }}</th>
-              <td>{{$user->pay_post_code}}</td>
-            </tr>
-            <tr>
-              <th scope="row">{{ Form::label('pay_address1', '請求書送付先（都道府県）') }}</th>
-              <td>{{$user->pay_address1}}</td>
-            </tr>
-            <tr>
-              <th scope="row">{{ Form::label('pay_address2', '請求書送付先（市町村番地）') }}</th>
-              <td>{{$user->pay_address2}}</td>
-            </tr>
-            <tr>
-              <th scope="row">{{ Form::label('pay_address3', '建物名') }}</th>
-              <td>{{$user->pay_address3}}</td>
-            </tr>
-            <tr>
-              <th scope="row">{{ Form::label('pay_remark', '請求書備考') }}</th>
-              <td>{{$user->pay_remark}}</td>
-            </tr>
-          </thead>
         </tbody>
       </table>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-sm">
-      <table class="table">
+
+      <!-- 支払いデータ ------------------------------------------------>
+      <table class="table table-bordered">
         <thead>
           <tr>
-            <th scope="col">注意事項</th>
+            <td colspan="3">
+              <p class="title-icon">
+                <i class="fas fa-yen-sign icon-size"></i>支払いデータ
+              </p>
+            </td>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <th scope="row">{{ Form::label('attention', '注意事項') }}</th>
-            <td>{{$user->attention}}</td>
+            <th class="table-active">{{ Form::label('pay_method', '支払方法') }}</th>
+            <td>
+              @if ($user->pay_method==1)
+              銀行振込
+              @elseif($user->pay_method==2)
+              現金
+              @elseif($user->pay_method==3)
+              クレジットカード
+              @elseif($user->pay_method==4)
+              スマホ決済
+              @endif
+            </td>
+          </tr>
+          <tr>
+            <th class="table-active">{{ Form::label('pay_limit', '支払期日') }}</th>
+            <td>
+              @if ($user->pay_limit==1)
+              当月末〆当月末CASH
+              @elseif($user->pay_limit==2)
+              当月末〆翌月末CASH
+              @elseif($user->pay_limit==3)
+              当月末〆翌々月末CACH
+              @elseif($user->pay_limit==4)
+              当月末〆3カ月末CASH
+              @endif
+            </td>
+          </tr>
+          <tr>
+            <th class="table-active">{{ Form::label('pay_post_code', '請求書送付先郵便番号') }}</th>
+            <td>{{$user->pay_post_code}}</td>
+          </tr>
+          <tr>
+            <th class="table-active">{{ Form::label('pay_address1', '請求書送付先（都道府県）') }}</th>
+            <td>{{$user->pay_address1}}</td>
+          </tr>
+          <tr>
+            <th class="table-active">{{ Form::label('pay_address2', '請求書送付先（市町村番地）') }}</th>
+            <td>{{$user->pay_address2}}</td>
+          </tr>
+          <tr>
+            <th class="table-active">{{ Form::label('pay_address3', '請求書送付先 (建物名)') }}</th>
+            <td>{{$user->pay_address3}}</td>
+          </tr>
+          <tr>
+            <th class="table-active">{{ Form::label('pay_remark', '請求書備考') }}</th>
+            <td>{{$user->pay_remark}}</td>
+          </tr>
+        </tbody>
+      </table>
+
+
+
+    </div>
+    <!-- 右側の項目　終わり -------------------------------------------------->
+  </div>
+
+  <!-- 備考 ----------------------------------------------------------->
+  <div class="row">
+    <div class="col">
+      <table class="table table table-bordered">
+        <thead>
+          <tr>
+          <th class="table-active caution">{{ Form::label('attention', '注意事項') }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="caution">{{$user->attention}}</td>
           </tr>
 
         </tbody>
       </table>
     </div>
-    <div class="col-sm">
-      <table class="table">
+    <div class="col">
+      <table class="table table table-bordered">
         <thead>
           <tr>
-            <th scope="col">備考</th>
+          <th class="table-active">{{ Form::label('remark', '備考') }}</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <th scope="row">{{ Form::label('remark', '備考') }}</th>
             <td>{{$user->remark}}</td>
           </tr>
           </thead>
@@ -196,21 +247,12 @@
       </table>
     </div>
   </div>
+  <!-- 　備考終わり ----------------------------------------->
+  
+  <div class="text-center mt-5">
+  <p><a class="more_btn_lg" href="">一覧にもどる</a>
+  </p>
+  </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-</div>
+</section>
 @endsection
