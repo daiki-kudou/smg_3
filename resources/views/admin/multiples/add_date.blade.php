93976d95a3e312e35648c0854887c2cb7c590def
daiki-kudou <67718513+daiki-kudou@users.noreply.github.com>
2021-03-10 19:29:32 +0900

    fix

---
+++ resources/views/admin/multiples/add_date.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.admin.app')
+
+@section('content')
+<link href="{{ asset('/css/template.css') }}" rel="stylesheet">
+<script src="{{ asset('/js/template.js') }}"></script>
+<script src="{{ asset('/js/multiples/calculate.js') }}"></script>
+
+
+
+
+@endsection
