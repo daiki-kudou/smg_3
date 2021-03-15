1f12885cc810ba24701edba051bd7a7963fd8b34
daiki-kudou <67718513+daiki-kudou@users.noreply.github.com>
2021-03-15 13:13:43 +0900

    fix

--- resources/views/admin/pre_reservations/index.blade.php
+++ resources/views/admin/pre_reservations/index.blade.php
@@ -230,8 +230,8 @@
             </td>
             <td>{{ReservationHelper::checkAgentOrUserTel($pre_reservation->user_id, $pre_reservation->agent_id)}}</td>
             <td>{{$pre_reservation->user_id!=999?"":$pre_reservation->unknown_user->unknown_user_company}}</td>
-            {{-- <td>{{$pre_reservation->agent_id==0?"":ReservationHelper::getAgentCompany($pre_reservation->agent_id)}}
-            </td> --}}
+            <td>{{$pre_reservation->agent_id==0?"":($pre_reservation->agent_id)}}
+            </td>
             <td>{{$pre_reservation->agent_id==0?"":$pre_reservation->pre_enduser->company}}</td>
             <td class="text-center">
               <a class="more_btn" href="{{url('admin/pre_reservations/'.$pre_reservation->id)}}">詳細</a></td>
