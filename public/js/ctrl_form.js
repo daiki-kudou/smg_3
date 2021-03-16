a01ea6ce14a2fddaa59a670b3b5e87b41e88f85a
daiki-kudou <67718513+daiki-kudou@users.noreply.github.com>
2021-03-16 22:41:07 +0900

    fix

--- public/js/ctrl_form.js
+++ public/js/ctrl_form.js
@@ -1,36 +1,59 @@
-// 文字、マイナス、数字制御
-function CTRLForm($target) {
-  jQuery(document).on('keydown', $target, function (e) {
-    let k = e.keyCode;
-    let str = String.fromCharCode(k);
-    if (!(str.match(/[0-9]/) || (37 <= k && k <= 40) || k === 8 || k === 46)) {
-      return false;
+
+$(function () {
+  function CTRLForm($inputName) {
+    $($inputName).on('input', function () {
+      check_numtype($(this), $inputName);
+    });
+  }
+  CTRLForm("input[name='capacity']");
+  CTRLForm("input[name='post_code']");
+  CTRLForm("input[name='luggage_post_code']");
+  CTRLForm("input[name='luggage_tel']");
+  CTRLForm("input[name='person_tel']");
+  CTRLForm("input[name='mgmt_tel']");
+  CTRLForm("input[name='mgmt_sec_company']");
+  CTRLForm("input[name='layout_prepare']");
+  CTRLForm("input[name='layout_clean']");
+  CTRLForm("input[name='stock']");
+  CTRLForm("input[name^='price']");
+  CTRLForm("input[name^='extend']");
+  CTRLForm("input[name^='time']");
+  CTRLForm("input[name='fax']");
+  CTRLForm("input[name='person_mobile']");
+  CTRLForm("input[name='cost']");
+  CTRLForm("input[name='tel']");
+  CTRLForm("input[name='pay_post_code']");
+  CTRLForm("input[name='mobile']");
+});
+
+// 文字制御開始
+var _chknum_value = "";
+function check_numtype(obj, inputName) {
+  var txt_obj = $(obj).val();
+  var text_length = txt_obj.length;
+  if (txt_obj.match(/^[0-9]+$/)) {
+    if (text_length > 9) {
+      $(inputName).val(_chknum_value);
+    } else {
+      _chknum_value = txt_obj;
     }
-  });
-  jQuery(document).on('keyup', $target, function (e) {
-    this.value = this.value.replace(/[^0-9]+/i, '');
-  });
-  jQuery(document).on('blur', $target, function () {
-    this.value = this.value.replace(/[^0-9]+/i, '');
-  });
+  } else {
+    if (text_length == 0) {
+      _chknum_value = "";
+    } else {
+      $(inputName).val(_chknum_value);
+    }
+  }
 }
 
-CTRLForm("input[name='capacity']");
-CTRLForm("input[name='post_code']");
-CTRLForm("input[name='luggage_post_code']");
-CTRLForm("input[name='luggage_tel']");
-CTRLForm("input[name='person_tel']");
-CTRLForm("input[name='mgmt_tel']");
-CTRLForm("input[name='mgmt_sec_company']");
-CTRLForm("input[name='layout_prepare']");
-CTRLForm("input[name='layout_clean']");
-CTRLForm("input[name='stock']");
-CTRLForm("input[name^='price']");
-CTRLForm("input[name^='extend']");
-CTRLForm("input[name^='time']");
-CTRLForm("input[name='fax']");
-CTRLForm("input[name='person_mobile']");
-CTRLForm("input[name='cost']");
-CTRLForm("input[name='tel']");
-CTRLForm("input[name='pay_post_code']");
-CTRLForm("input[name='mobile']");
+
+$(function () {
+  function resetPostCode($inputName) {
+    $($inputName).on("click", function () {
+      $($inputName).val("");
+    })
+  }
+  resetPostCode('input[name="post_code"]');
+  resetPostCode('input[name="luggage_post_code"]');
+})
+
