807b04bce561742f3ecaac9b08678212ea90747b
daiki-kudou <67718513+daiki-kudou@users.noreply.github.com>
2021-03-17 19:17:36 +0900

    fix

--- resources/views/admin/frame_prices/show.blade.php
+++ resources/views/admin/frame_prices/show.blade.php
@@ -18,7 +18,9 @@
 
 <section class="section-wrap bg-white wrap_shadow">
   @if (count($frame_prices)==0 && count($time_prices)==0)
-  <h3 class="d-block"><span class="mr-3">ID:{{ ReservationHelper::IdFormat($venue->id)}}</span>{{$venue->name_area}}・{{$venue->name_bldg}}{{$venue->name_venue}}</h3>
+  <h3 class="d-block"><span
+      class="mr-3">ID:{{ ReservationHelper::IdFormat($venue->id)}}</span>{{$venue->name_area}}・{{$venue->name_bldg}}{{$venue->name_venue}}
+  </h3>
   <span class="mt-5 mb-5 d-block">※料金データが登録されていません</span>
   <div class="d-flex justify-content-around">
     <div>
@@ -38,14 +40,14 @@
 
     <h4 class="mt-5">料金体系：通常(枠貸し料金)</h4>
     <div class="d-flex justify-content-between align-items-center mt-3">
-    <p class="mb-2">・枠は「午前」「午後」「夜間」「午前＆午後」「午後＆夜間」「終日」です。</p>
-    <div class="text-right mb-2">
-      @if (!count($frame_prices)==0)
-      {{ link_to_route('admin.frame_prices.edit', '枠貸し編集', $parameters=$venue->id,['class' => 'btn more_btn']) }}
-      @else
-      {{ link_to_route('admin.frame_prices.create', '枠貸し新規登録', $parameters=$venue->id,['class' => 'btn more_btn']) }}
-      @endif
-    </div>
+      <p class="mb-2">・枠は「午前」「午後」「夜間」「午前＆午後」「午後＆夜間」「終日」です。</p>
+      <div class="text-right mb-2">
+        @if (!count($frame_prices)==0)
+        {{ link_to_route('admin.frame_prices.edit', '枠貸し編集', $parameters=$venue->id,['class' => 'btn more_btn']) }}
+        @else
+        {{ link_to_route('admin.frame_prices.create', '枠貸し新規登録', $parameters=$venue->id,['class' => 'btn more_btn']) }}
+        @endif
+      </div>
     </div>
     <table class="table table-bordered">
       <thead>
@@ -53,6 +55,7 @@
           <th class="table-active" scope="col">枠</th>
           <th class="table-active" scope="col">時間</th>
           <th class="table-active" scope="col">料金<span class="ml-1 annotation">※税抜</span></th>
+          <th class="table-active" scope="col">延長料金</th>
         </tr>
       </thead>
       <tbody>
@@ -63,6 +66,9 @@
             {{Carbon\Carbon::parse($frame_price->finish)->format('H:i')}}　（{{Carbon\Carbon::parse($frame_price->finish)->diffInHours(Carbon\Carbon::parse($frame_price->start))}}H）
           </td>
           <td>{{ number_format($frame_price->price)}}円</td>
+          @if ($loop->first)
+          <td rowspan={{$frame_prices->count()}}>test</td>
+          @endif
         </tr>
         @endforeach
       </tbody>
@@ -70,34 +76,34 @@
   </div>
 
   <div class="pt-5">
-        <h4 class="mb-3">料金体系：アクセア仕様(時間貸し料金)</h4>
-      <div class="text-right mb-2">
-        @if (!count($time_prices)==0)
-        {{ link_to_route('admin.time_prices.edit', '時間貸し編集', $parameters=$venue->id,['class' => 'btn more_btn']) }}
-        @else
-        {{ link_to_route('admin.time_prices.create', '時間貸し新規登録', $parameters=$venue->id,['class' => 'btn more_btn']) }}
-        @endif
-      </div>
-      <table class="table table-bordered">
-        <thead>
-          <tr>
-            <th class="table-active" scope="col">時間</th>
-            <th class="table-active" scope="col">料金<span class="ml-1 annotation">※税抜</span></th>
-            <th class="table-active" scope="col">延長料金<span class="ml-1 annotation">※税抜</span></th>
-            <!-- <th scope="col">登録日</th> -->
-          </tr>
-        </thead>
-        <tbody>
-          @foreach ($time_prices as $time_price)
-          <tr>
-            <th>{{ $time_price->time}}</th>
-            <td>{{ number_format($time_price->price)}}円</td>
-            <td>{{ number_format($time_price->extend)}}円</td>
-            <!-- <td>{{ ReservationHelper::formatDate($time_price->created_at)}}</td> -->
-          </tr>
-          @endforeach
-        </tbody>
-      </table>
+    <h4 class="mb-3">料金体系：アクセア仕様(時間貸し料金)</h4>
+    <div class="text-right mb-2">
+      @if (!count($time_prices)==0)
+      {{ link_to_route('admin.time_prices.edit', '時間貸し編集', $parameters=$venue->id,['class' => 'btn more_btn']) }}
+      @else
+      {{ link_to_route('admin.time_prices.create', '時間貸し新規登録', $parameters=$venue->id,['class' => 'btn more_btn']) }}
+      @endif
+    </div>
+    <table class="table table-bordered">
+      <thead>
+        <tr>
+          <th class="table-active" scope="col">時間</th>
+          <th class="table-active" scope="col">料金<span class="ml-1 annotation">※税抜</span></th>
+          <th class="table-active" scope="col">延長料金<span class="ml-1 annotation">※税抜</span></th>
+          <!-- <th scope="col">登録日</th> -->
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($time_prices as $time_price)
+        <tr>
+          <th>{{ $time_price->time}}</th>
+          <td>{{ number_format($time_price->price)}}円</td>
+          <td>{{ number_format($time_price->extend)}}円</td>
+          <!-- <td>{{ ReservationHelper::formatDate($time_price->created_at)}}</td> -->
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
   </div>
   @endif
 
