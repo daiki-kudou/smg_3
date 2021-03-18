04a94bf5e5ceff01247c4845ce4b39c8bd80d50a
maru-star <maruoka@web-trickster.com>
2021-03-18 05:51:26 +0000

    service equipment table fix

--- resources/views/admin/services/edit.blade.php
+++ resources/views/admin/services/edit.blade.php
@@ -16,61 +16,64 @@
 </div>
 @endif
 
-  <div class="float-right">
-    <nav aria-label="breadcrumb">
-      <ol class="breadcrumb">
-        <li class="breadcrumb-item active">
-          {{ Breadcrumbs::render(Route::currentRouteName(),$service->id) }}
-        </li>
-      </ol>
-    </nav>
-  </div>
+<div class="float-right">
+  <nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item active">
+        {{ Breadcrumbs::render(Route::currentRouteName(),$service->id) }}
+      </li>
+    </ol>
+  </nav>
+</div>
 
-  <h2 class="mt-3 mb-3">有料サービス管理　編集</h2>
-  <hr>
+<h2 class="mt-3 mb-3">有料サービス管理　編集</h2>
+<hr>
 
 
-  {{ Form::model($service, ['route' => ['admin.services.update', $service->id], 'method' => 'put', 'id'=>'ServiceUpdateForm']) }}
-  @csrf
+{{ Form::model($service, ['route' => ['admin.services.update', $service->id], 'method' => 'put', 'id'=>'ServiceUpdateForm']) }}
+@csrf
 
-  <p class="text-right mt-5">※金額は税抜で入力してください。</p>
-  <table class="table table-striped table-bordered ">
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>登録日</th>
-        <th class="form_required">サービス名</th>
-        <th class="form_required">料金<span class="ml-1 annotation">※税抜</span></th>
-        <th>備考</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>{{ ReservationHelper::IdFormat($service->id) }}</td>
-        <td>{{ ($service->created_at) }}</td>
-        <td>
-          {{ Form::text('item', $service->item, ['class' => 'form-control']) }}
-          <p class="is-error-item" style="color: red"></p>
-        </td>
-        <td>
+<p class="text-right mt-5">※金額は税抜で入力してください。</p>
+<table class="table table-striped table-bordered ">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>登録日</th>
+      <th class="form_required">サービス名</th>
+      <th class="form_required">料金<span class="ml-1 annotation">※税抜</span></th>
+      <th>備考</th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>{{ ReservationHelper::IdFormat($service->id) }}</td>
+      <td>{{ ($service->created_at) }}</td>
+      <td>
+        {{ Form::text('item', $service->item, ['class' => 'form-control']) }}
+        <p class="is-error-item" style="color: red"></p>
+      </td>
+      <td>
+        <div class="d-flex align-items-end">
           {{ Form::text('price', $service->price, ['class' => 'form-control']) }}
-          <p class="is-error-price" style="color: red"></p>
-        </td>
-        <td>
-          {{ Form::textarea('remark', $service->remark, ['class' => 'form-control','rows'=>"2"]) }}
-        </td>
-        <td>
-          {{ Form::submit('更新', ['class' => 'btn more_btn approval']) }}
-          <div class="loading hide">
-            <button class="btn more_btn" type="button" disabled>
-              <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-            </button>
-          </div>
-        </td>
-      </tr>
-    </tbody>
-  </table>
-  </form>
+          <span class="ml-1">円</span>
+        </div>
+        <p class="is-error-price" style="color: red"></p>
+      </td>
+      <td>
+        {{ Form::textarea('remark', $service->remark, ['class' => 'form-control','rows'=>"2"]) }}
+      </td>
+      <td>
+        {{ Form::submit('更新', ['class' => 'btn more_btn approval']) }}
+        <div class="loading hide">
+          <button class="btn more_btn" type="button" disabled>
+            <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+          </button>
+        </div>
+      </td>
+    </tr>
+  </tbody>
+</table>
+</form>
 
 @endsection
