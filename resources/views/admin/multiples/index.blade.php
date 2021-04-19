7fa2ad61d8030599ebe1da08dda31116b934deff
daiki-kudou <67718513+daiki-kudou@users.noreply.github.com>
2021-04-19 15:01:25 +0900

    fix

--- resources/views/admin/multiples/index.blade.php
+++ resources/views/admin/multiples/index.blade.php
@@ -179,52 +179,53 @@
                 <input type="checkbox" name="{{'delete_check'.$multiple->id}}" value="{{$multiple->id}}"
                   class="checkbox" />
               </td>
+
               <td>{{ReservationHelper::fixId($multiple->id)}}</td>
               <td>{{ReservationHelper::formatDate($multiple->created_at)}}</td>
               <td>{{$multiple->pre_reservations->count()}}</td>
               <td>
-                @if ($multiple->pre_reservations->first()->user->id!=0)
+                @if (!empty($multiple->pre_reservations->first()->user))
                 {{(ReservationHelper::getCompany($multiple->pre_reservations->first()->user->id))}}
                 @endif
               </td>
               <td>
-                @if ($multiple->pre_reservations->first()->user->id!=0)
+                @if (!empty($multiple->pre_reservations->first()->user))
                 {{ReservationHelper::getPersonName($multiple->pre_reservations->first()->user->id)}}
                 @else
                 {{ReservationHelper::getAgentPerson($multiple->pre_reservations->first()->agent_id)}}
                 @endif
               </td>
               <td>
-                @if ($multiple->pre_reservations->first()->user->id!=0)
+                @if (!empty($multiple->pre_reservations->first()->user))
                 {{ReservationHelper::getPersonMobile($multiple->pre_reservations->first()->user->id)}}
                 @else
                 {{ReservationHelper::getAgentMobile($multiple->pre_reservations->first()->agent_id)}}
                 @endif
               </td>
               <td>
-                @if ($multiple->pre_reservations->first()->user->id!=0)
+                @if (!empty($multiple->pre_reservations->first()->user))
                 {{ReservationHelper::getPersonTel($multiple->pre_reservations->first()->user->id)}}
                 @else
                 {{ReservationHelper::getAgentTel($multiple->pre_reservations->first()->agent_id)}}
                 @endif
               </td>
               <td>
-                @if ($multiple->pre_reservations->first()->user->id!=0)
+                @if (!empty($multiple->pre_reservations->first()->user))
                 {{($multiple->pre_reservations->first()->unknown_user->unknown_user_company)}}
                 @endif
               </td>
               <td>
-                @if ($multiple->pre_reservations->first()->agent_id!=0)
-                {{(ReservationHelper::getAgentCompanyName($multiple->pre_reservations->first()->agent_id))}}
+                @if (empty($multiple->pre_reservations->first()->user))
+                {{$multiple->pre_reservations->first()->agent->company}}
                 @endif
               </td>
               <td>
-                @if ($multiple->pre_reservations->first()->agent_id!=0)
-                {{!empty($multiple->pre_reservations->first()->pre_enduser->company)?$multiple->pre_reservations->first()->pre_enduser->company:""}}
+                @if (empty($multiple->pre_reservations->first()->user))
+                {{$multiple->pre_reservations->first()->pre_enduser->company}}
                 @endif
               </td>
               <td class="text-center">
-                @if ($multiple->pre_reservations->first()->user->id!=0)
+                @if (!empty($multiple->pre_reservations->first()->user))
                 <a href="{{url('admin/multiples/'.$multiple->id)}}" class="btn more_btn">詳細</a>
                 @else
                 <a href="{{url('admin/multiples/agent/'.$multiple->id)}}" class="btn more_btn">詳細</a>
