27d55ca0507a9ba378e263e87dcf26c5a90a3e8c
daiki-kudou <67718513+daiki-kudou@users.noreply.github.com>
2021-04-19 14:29:09 +0900

    fix

--- resources/views/admin/multiples/index.blade.php
+++ resources/views/admin/multiples/index.blade.php
@@ -174,65 +174,64 @@
           </thead>
           <tbody>
             @foreach ($multiples as $multiple)
-            <pre>{{var_dump($multiple->pre_reservations->first()->user_id)}}</pre>
-            {{-- <tr>
+            <pre>{{var_dump(ReservationHelper::getCompany($multiple->pre_reservations->first()->user_id))}}</pre>
+            <tr>
               <td class="text-center">
                 <input type="checkbox" name="{{'delete_check'.$multiple->id}}" value="{{$multiple->id}}"
-            class="checkbox" />
-            </td>
-
-            <td>{{ReservationHelper::fixId($multiple->id)}}</td>
-            <td>{{ReservationHelper::formatDate($multiple->created_at)}}</td>
-            <td>{{$multiple->pre_reservations->count()}}</td>
-            <td>
-              @if ($multiple->pre_reservations->first()->user_id!=0)
-              {{(ReservationHelper::getCompany($multiple->pre_reservations->first()->user_id))}}
-              @endif
-            </td>
-            <td>
-              @if ($multiple->pre_reservations->first()->user_id!=0)
-              {{ReservationHelper::getPersonName($multiple->pre_reservations->first()->user_id)}}
-              @else
-              {{ReservationHelper::getAgentPerson($multiple->pre_reservations->first()->agent_id)}}
-              @endif
-            </td>
-            <td>
-              @if ($multiple->pre_reservations->first()->user_id!=0)
-              {{ReservationHelper::getPersonMobile($multiple->pre_reservations->first()->user_id)}}
-              @else
-              {{ReservationHelper::getAgentMobile($multiple->pre_reservations->first()->agent_id)}}
-              @endif
-            </td>
-            <td>
-              @if ($multiple->pre_reservations->first()->user_id!=0)
-              {{ReservationHelper::getPersonTel($multiple->pre_reservations->first()->user_id)}}
-              @else
-              {{ReservationHelper::getAgentTel($multiple->pre_reservations->first()->agent_id)}}
-              @endif
-            </td>
-            <td>
-              @if ($multiple->pre_reservations->first()->user_id!=0)
-              {{($multiple->pre_reservations->first()->unknown_user->unknown_user_company)}}
-              @endif
-            </td>
-            <td>
-              @if ($multiple->pre_reservations->first()->agent_id!=0)
-              {{(ReservationHelper::getAgentCompanyName($multiple->pre_reservations->first()->agent_id))}}
-              @endif
-            </td>
-            <td>
-              @if ($multiple->pre_reservations->first()->agent_id!=0)
-              {{!empty($multiple->pre_reservations->first()->pre_enduser->company)?$multiple->pre_reservations->first()->pre_enduser->company:""}}
-              @endif
-            </td>
-            <td class="text-center">
-              @if ($multiple->pre_reservations->first()->user_id!=0)
-              <a href="{{url('admin/multiples/'.$multiple->id)}}" class="btn more_btn">詳細</a>
-              @else
-              <a href="{{url('admin/multiples/agent/'.$multiple->id)}}" class="btn more_btn">詳細</a>
-              @endif
-            </td>
-            </tr> --}}
+                  class="checkbox" />
+              </td>
+              <td>{{ReservationHelper::fixId($multiple->id)}}</td>
+              <td>{{ReservationHelper::formatDate($multiple->created_at)}}</td>
+              <td>{{$multiple->pre_reservations->count()}}</td>
+              <td>
+                @if ($multiple->pre_reservations->first()->user_id!=0)
+                {{(ReservationHelper::getCompany($multiple->pre_reservations->first()->user_id))}}
+                @endif
+              </td>
+              <td>
+                @if ($multiple->pre_reservations->first()->user_id!=0)
+                {{ReservationHelper::getPersonName($multiple->pre_reservations->first()->user_id)}}
+                @else
+                {{ReservationHelper::getAgentPerson($multiple->pre_reservations->first()->agent_id)}}
+                @endif
+              </td>
+              <td>
+                @if ($multiple->pre_reservations->first()->user_id!=0)
+                {{ReservationHelper::getPersonMobile($multiple->pre_reservations->first()->user_id)}}
+                @else
+                {{ReservationHelper::getAgentMobile($multiple->pre_reservations->first()->agent_id)}}
+                @endif
+              </td>
+              <td>
+                @if ($multiple->pre_reservations->first()->user_id!=0)
+                {{ReservationHelper::getPersonTel($multiple->pre_reservations->first()->user_id)}}
+                @else
+                {{ReservationHelper::getAgentTel($multiple->pre_reservations->first()->agent_id)}}
+                @endif
+              </td>
+              <td>
+                @if ($multiple->pre_reservations->first()->user_id!=0)
+                {{($multiple->pre_reservations->first()->unknown_user->unknown_user_company)}}
+                @endif
+              </td>
+              <td>
+                @if ($multiple->pre_reservations->first()->agent_id!=0)
+                {{(ReservationHelper::getAgentCompanyName($multiple->pre_reservations->first()->agent_id))}}
+                @endif
+              </td>
+              <td>
+                @if ($multiple->pre_reservations->first()->agent_id!=0)
+                {{!empty($multiple->pre_reservations->first()->pre_enduser->company)?$multiple->pre_reservations->first()->pre_enduser->company:""}}
+                @endif
+              </td>
+              <td class="text-center">
+                @if ($multiple->pre_reservations->first()->user_id!=0)
+                <a href="{{url('admin/multiples/'.$multiple->id)}}" class="btn more_btn">詳細</a>
+                @else
+                <a href="{{url('admin/multiples/agent/'.$multiple->id)}}" class="btn more_btn">詳細</a>
+                @endif
+              </td>
+            </tr>
 
             @endforeach
           </tbody>
