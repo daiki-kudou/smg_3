f03bfebb34476cd2c317cf3e0c4ac7238e06f686
maru-star <maruoka@web-trickster.com>
2021-04-28 12:03:36 +0000

    invoice add

--- resources/views/user/pre_reservations/calculate.blade.php
+++ resources/views/user/pre_reservations/calculate.blade.php
@@ -353,6 +353,40 @@
             </tbody>
           </table>
         </div>
+        <div class="eat_in">
+            <table class="table table-bordered">
+              <thead>
+                <tr>
+                  <th colspan='2'>
+                    <p class="title-icon">
+                      <i class="fas fa-utensils icon-size fa-fw"></i>工藤さん！！！こちらお願いします。室内飲食
+                    </p>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <input id="eat_in" name="eat_in" type="radio" value="1">
+                    <label for="eat_in">あり</label>
+                  </td>
+                  <td>
+                    <input id="eat_in_prepare" disabled name="eat_in_prepare" type="radio" value="1">
+                    <label for="eat_in_prepare">手配済み</label>
+                    <input id="eat_in_consider" disabled name="eat_in_prepare" type="radio" value="2">
+                    <label for="eat_in_consider">検討中</label>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <input id="no_eat_in" checked="checked" name="eat_in" type="radio" value="0">
+                    <label for="no_eat_in">なし</label>
+                  </td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
 
 
         <table class="table table-bordered note-table">
