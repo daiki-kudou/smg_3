3e89d17e98497dac608d4d52402cf90e32b39fc5
daiki <daikikudoh313@gmail.com>
2021-05-06 19:14:06 +0900

    fix

--- resources/views/admin/calendar/venue_calendar.blade.php
+++ resources/views/admin/calendar/venue_calendar.blade.php
@@ -11,16 +11,16 @@
 @foreach ($days as $key=>$day)
 @foreach ($reservations as $reservation)
 @if ($reservation->reserve_date==$day)
-{{Form::text('start', date('Y-m-d',strtotime($reservation->reserve_date)).' '.$reservation->enter_time,['id'=>date('Y-m-d',strtotime($day)).'start'])}}
-{{Form::text('finish', date('Y-m-d',strtotime($reservation->reserve_date)).' '.$reservation->leave_time,['id'=>date('Y-m-d',strtotime($day)).'finish'])}}
-{{Form::text('date', date('Y-m-d',strtotime($reservation->reserve_date)))}}
-{{Form::text('status', $reservation->bills()->first()->reservation_status)}}
+{{Form::hidden('start', date('Y-m-d',strtotime($reservation->reserve_date)).' '.$reservation->enter_time,['id'=>date('Y-m-d',strtotime($day)).'start'])}}
+{{Form::hidden('finish', date('Y-m-d',strtotime($reservation->reserve_date)).' '.$reservation->leave_time,['id'=>date('Y-m-d',strtotime($day)).'finish'])}}
+{{Form::hidden('date', date('Y-m-d',strtotime($reservation->reserve_date)))}}
+{{Form::hidden('status', $reservation->bills()->first()->reservation_status)}}
 @if ($reservation->user_id>0)
-{{Form::text('company', ReservationHelper::getCompany($reservation->user_id))}}
+{{Form::hidden('company', ReservationHelper::getCompany($reservation->user_id))}}
 @else
-{{Form::text('company', ReservationHelper::getAgentCompany($reservation->agent_id))}}
+{{Form::hidden('company', ReservationHelper::getAgentCompany($reservation->agent_id))}}
 @endif
-{{Form::text('reservation_id', $reservation->id)}}
+{{Form::hidden('reservation_id', $reservation->id)}}
 @endif
 @endforeach
 
