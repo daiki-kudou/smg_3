fc3473544c25f5f6408a936953ee9432536179f3
daiki-kudou <67718513+daiki-kudou@users.noreply.github.com>
2021-05-17 18:06:01 +0900

    fix

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -143,7 +143,7 @@ class HomeController extends Controller
           'tel' => $request->tel,
           'mobile' => $request->mobile,
           'fax' => $request->fax,
-          'email' => $request->email,
+          // 'email' => $request->email,
         ]);
       } catch (\Exception $e) {
         report($e);
