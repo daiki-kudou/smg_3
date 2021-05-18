04a67ac40b10faa3bd3f01dea84a279c9213c120
daiki-kudou <67718513+daiki-kudou@users.noreply.github.com>
2021-05-18 21:19:00 +0900

    fix

--- app/Notifications/CustomResetPassword.php
+++ app/Notifications/CustomResetPassword.php
@@ -48,12 +48,17 @@ class CustomResetPassword extends Notification
     // }
 
     return (new MailMessage)
-      ->subject(Lang::get('Reset Password Notification'))
-      ->line(Lang::get('You are receiving this email because we received a password reset request for your account.'))
-      // 以下修正　url(route('password.reset　から　url(route('user.password.resetへ
-      ->action(Lang::get('Reset Password'), url(route('user.password.reset', ['token' => $this->token, 'email' => $notifiable->getEmailForPasswordReset()], false)))
-      ->line(Lang::get('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.' . config('auth.defaults.passwords') . '.expire')]))
-      ->line(Lang::get('If you did not request a password reset, no further action is required.'));
+      // ->subject(Lang::get('Reset Password Notification'))
+      // ->line(Lang::get('You are receiving this email because we received a password reset request for your account.'))
+      // // 以下修正　url(route('password.reset　から　url(route('user.password.resetへ
+      // ->action(Lang::get('Reset Password'), url(route('user.password.reset', ['token' => $this->token, 'email' => $notifiable->getEmailForPasswordReset()], false)))
+      // ->line(Lang::get('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.' . config('auth.defaults.passwords') . '.expire')]))
+      // ->line(Lang::get('If you did not request a password reset, no further action is required.'));
+      ->from('kudou@web-trickster.com', config('app.name'))
+      ->subject('パスワード再設定')
+      ->line('下のボタンをクリックしてパスワードを再設定してください。')
+      ->action('パスワード再設定', url(config('app.url') . route('user.password.reset', $this->token, false)))
+      ->line('もし心当たりがない場合は、本メッセージは破棄してください。');
   }
 
   /**
