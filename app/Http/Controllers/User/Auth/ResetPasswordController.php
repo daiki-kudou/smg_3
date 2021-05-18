638e1a7190c7a363f4013180f41b46c4fba26322
daiki-kudou <67718513+daiki-kudou@users.noreply.github.com>
2021-05-18 20:12:59 +0900

    fix

--- app/Http/Controllers/User/Auth/ResetPasswordController.php
+++ app/Http/Controllers/User/Auth/ResetPasswordController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
+use Illuminate\Http\Request;
+
+
 class ResetPasswordController extends Controller
 {
   /*
