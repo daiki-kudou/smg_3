8e9d20251e9bbed6c807261b7b06b661ded48676
daiki-kudou <67718513+daiki-kudou@users.noreply.github.com>
2021-06-04 18:30:17 +0900

    fix agian

--- resources/views/admin/reservations/show/user/right_column.blade.php
+++ resources/views/admin/reservations/show/user/right_column.blade.php
@@ -126,7 +126,6 @@
       <td>
         <p>管理者備考</p>
         <p>{{isset($reservation->admin_details)?$reservation->admin_details:'なし'}}</p>
-        <!-- <p>{!!nl2br(e($reservation->admin_details))!!}</p> -->
       </td>
     </tr>
   </table>
