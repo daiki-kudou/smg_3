5d261856a8d87c04e295a1b64e6e6e483d0714e4
maru-star <maruoka@web-trickster.com>
2021-06-17 09:30:55 +0000

    venues validation structure change

--- resources/views/admin/venues/edit.blade.php
+++ resources/views/admin/venues/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <script src="{{ asset('/js/template.js') }}"></script>
-<script src="{{ asset('/js/admin/validation.js') }}"></script>
+<script src="{{ asset('/js/admin/venues/validation.js') }}"></script>
 <link href="{{ asset('/css/template.css') }}" rel="stylesheet">
 <script src="{{ asset('/js/admin/reservation.js') }}"></script>
 <script src="{{ asset('/js/ctrl_form.js') }}"></script>
