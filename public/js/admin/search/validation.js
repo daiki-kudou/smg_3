4b6a4c35510a9ac85b118930177fdee9a3d3e1cd
maru-star <maruoka@web-trickster.com>
2021-06-17 10:05:07 +0000

    index search validation file bulk

--- public/js/admin/search/validation.js
+++ public/js/admin/search/validation.js
@@ -38,19 +38,6 @@ jQuery.validator.addMethod(
 );
 
 
-// 有料備品の数字入力制限
-$(function () {
-  $(".equipment_validation").on("input", function (e) {
-    let value = $(e.currentTarget).val();
-    value = value
-      .replace(/[０-９]/g, function (s) {
-        return String.fromCharCode(s.charCodeAt(0) - 65248);
-      })
-      .replace(/[^0-9]/g, "");
-    $(e.currentTarget).val(value);
-  });
-});
-
 // その他の数字入力制限
 $(function () {
   $("input[name*='others_input_cost']").on("input", function (e) {
@@ -77,69 +64,31 @@ $(function () {
   });
 });
 
-
-
-
-
-
-// 備品のカウントイッタン非表示
-// $(function () {
-//   var items = $(".equipment_breakdown");
-
-//   for (var i=0; i<items.length; i++) {
-//     console.log(i);
-
-//     var equipment_breakdown = "equipment_breakdown" + i;
-//     $("input[name='equipment_breakdown']").on("input", function (e) {
-//       let value = $(e.currentTarget).val();
-//       value = value
-//         .replace(/[０-９]/g, function (s) {
-//           return String.fromCharCode(s.charCodeAt(0) - 65248);
-//         })
-//         .replace(/[^0-9]/g, "");
-//       $(e.currentTarget).val(value);
-//     });
-
-//   }
-
-
-// });
-
-// マイページvalidation
-
+//検索
 $(function () {
-  var target = [
-    "#mypageForm", "#mypageDone",
-  ];
-
+  var target = ["#preserve_search", "#multiples_search"
+    , "#clients_search", "#reserve_search"];
   $.each(target, function (index, value) {
     $(value).validate({
       rules: {
-        in_charge: { required: true },
-        tel: { required: true, number: true, minlength: 11 },
-        luggage_count: { number: true, range: [0, 49] },
-        luggage_return: { number: true, range: [0, 49] },
-        luggage_price: { number: true },
-
+        multiple_id: { number: true },
+        search_id: { number: true },
+        search_mobile: { number: true },
+        search_tel: { number: true },
+        search_email: { sign: true },
+        id: { number: true },
+        mobile: { number: true },
+        tel: { number: true },
       },
       messages: {
-        in_charge: { required: "※必須項目です" },
-        tel: {
-          required: "※必須項目です",
-          number: "※半角数字を入力してください",
-          minlength: "※最低桁数は11です",
-        },
-        luggage_count: {
-          number: "半角数字で入力してください",
-          range: "※最大値は49です",
-        },
-        luggage_return: {
-          number: "※半角数字で入力してください",
-          range: "※最大値は49です",
-        },
-        luggage_price: {
-          number: "※半角数字で入力してください",
-        },
+        multiple_id: { number: "※半角数字を入力してください" },
+        search_id: { number: "※半角数字を入力してください" },
+        search_mobile: { number: "※半角数字を入力してください" },
+        search_tel: { number: "※半角数字を入力してください" },
+        search_email: { sign: "※半角英数字を入力してください" },
+        id: { number: "※半角数字を入力してください" },
+        mobile: { number: "※半角数字を入力してください" },
+        tel: { number: "※半角数字を入力してください" },
       },
       errorPlacement: function (error, element) {
         var name = element.attr("name");
@@ -164,3 +113,49 @@ $(function () {
   });
 });
 
+// 一括仮押さえ一覧　検索
+$(function () {
+  $("#searchMultiple").validate({
+    rules: {
+      search_id: { number: true, min: 1 },
+      search_mobile: { number: true, minlength: 11 },
+      search_tel: { number: true, minlength: 10 },
+    },
+    messages: {
+      search_id: {
+        number: "※半角英数字で入力してください",
+        min: "※0以上を入力してください",
+      },
+      search_mobile: {
+        number: "※半角英数字で入力してください",
+        minlength: "11桁で入力してください",
+      },
+      search_tel: {
+        number: "※半角英数字で入力してください",
+        minlength: "10桁で入力してください",
+      },
+    },
+    errorPlacement: function (error, element) {
+      var name = element.attr("name");
+      if (element.attr("name") === "category[]") {
+        error.appendTo($(".is-error-category"));
+      } else if (element.attr("name") === name) {
+        error.appendTo($(".is-error-" + name));
+      }
+    },
+    errorElement: "span",
+    errorClass: "is-error",
+    //送信前にLoadingを表示
+    submitHandler: function (form) {
+      $(".approval").addClass("hide");
+      $(".loading").removeClass("hide");
+      form.submit();
+    },
+  });
+  $("input").on("blur", function () {
+    $(this).valid();
+    // if ($('span').hasClass('is-error')) {
+    //   $('span').css('background', 'white');
+    // }
+  });
+});
