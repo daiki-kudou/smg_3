04a21e63c9561ff6d83459ed310f95f7743dd888
maru-star <maruoka@web-trickster.com>
2021-06-17 05:50:28 +0000

    validation structure fix and resign membe

--- resources/views/admin/multiples/edit.blade.php
+++ resources/views/admin/multiples/edit.blade.php
@@ -3,6 +3,7 @@
 <link href="{{ asset('/css/template.css') }}" rel="stylesheet">
 <script src="{{ asset('/js/template.js') }}"></script>
 <script src="{{ asset('/js/lettercounter.js') }}"></script>
+<script src="{{ asset('/js/admin/multiples/validation.js') }}"></script>
 <script src="{{ asset('/js/multiples/calculate.js') }}"></script>
 {{--<script src="{{ asset('/js/admin/validation.js') }}"></script>--}}
 
