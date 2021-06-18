7243e75f8d86fcd9d029db833f5ff2da967420a0
maru-star <maruoka@web-trickster.com>
2021-06-18 06:09:40 +0000

    reservation edit

--- resources/views/admin/reservations/edit.blade.php
+++ resources/views/admin/reservations/edit.blade.php
@@ -6,7 +6,7 @@
 <script src="{{ asset('/js/template.js') }}"></script>
 <script src="{{ asset('/js/ajax.js') }}"></script>
 <script src="{{ asset('/js/lettercounter.js') }}"></script>
-<script src="{{ asset('/js/admin/validation.js') }}"></script>
+<script src="{{ asset('/js/admin/reservation/validation.js') }}"></script>
 
 
 
