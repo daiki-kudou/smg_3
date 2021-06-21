9405c0cd33e2dc951000769c3e2db0a721fd9cf8
maru-star <maruoka@web-trickster.com>
2021-06-21 05:47:40 +0000

    prereserve notaion add

--- resources/views/admin/multiples/edit.blade.php
+++ resources/views/admin/multiples/edit.blade.php
@@ -1179,6 +1179,8 @@
                 </div>
                 <div class="main">
                   <div class="venues billdetails_content">
+                    {{-- 工藤さん！！！！！下記、注釈追加です。 --}}
+                   <p class="mb-2">※営業時間外の予約の為、管理者側にて料金設定を行います。</p>
                     <table class="table table-borderless">
                       <tr>
                         <td>
