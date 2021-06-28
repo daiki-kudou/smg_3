4c6b9fc25c2e4c66fd2f8e75cf1c20b8c62a3006
daiki-kudou <67718513+daiki-kudou@users.noreply.github.com>
2021-06-28 18:57:55 +0900

    fix

--- app/Http/Controllers/Admin/FakeTestController.php
+++ app/Http/Controllers/Admin/FakeTestController.php
@@ -13,6 +13,7 @@ class FakeTestController extends Controller
 {
   public function index()
   {
+    $this->calc(17, 2, '08:00:00', '23:00:00', 74500);
   }
 
   public function calc($venueId, $priceSystem, $enter, $leave, $expectValue)
@@ -21,19 +22,19 @@ class FakeTestController extends Controller
     $result = $venue->calculate_price($priceSystem, $enter, $leave);
 
     if ($result === 0) {
-      // echo "";
-      // echo "会場ID: " . $venueId . ' / ' . '料金体系' . $priceSystem . ' / ' . '入室: ' . $enter . ' / ' . '退室 :' . $leave . ' / ' . '期待する結果: ' . $expectValue . '<br>';
-      // echo "↓　システム算出結果<br><div style='background:black; color:#56DB3A;font-weight:bold;'>";
-      // dump(0);
-      // echo "</div><br>";
+      echo "";
+      echo "会場ID: " . $venueId . ' / ' . '料金体系' . $priceSystem . ' / ' . '入室: ' . $enter . ' / ' . '退室 :' . $leave . ' / ' . '期待する結果: ' . $expectValue . '<br>';
+      echo "↓　システム算出結果<br><div style='background:black; color:#56DB3A;font-weight:bold;'>";
+      dump(0);
+      echo "</div><br>";
     } elseif ($result[0] == $expectValue) {
-      // echo "";
-      // echo "会場ID: " . $venueId . ' / ' . '料金体系' . $priceSystem . ' / ' . '入室: ' . $enter . ' / ' . '退室 :' . $leave . ' / ' . '期待する結果: ' . $expectValue . '<br>';
-      // echo "↓　システム算出結果<br><div style='background:black; color:#56DB3A;font-weight:bold;'>";
-      // echo "会場料金: " . ($result[0] - $result[1]) . '<br>';
-      // echo "延長料金: " . $result[1] . '<br>';
-      // echo "合計料金: " . $result[0] . '<br>';
-      // echo "</div><br>";
+      echo "";
+      echo "会場ID: " . $venueId . ' / ' . '料金体系' . $priceSystem . ' / ' . '入室: ' . $enter . ' / ' . '退室 :' . $leave . ' / ' . '期待する結果: ' . $expectValue . '<br>';
+      echo "↓　システム算出結果<br><div style='background:black; color:#56DB3A;font-weight:bold;'>";
+      echo "会場料金: " . ($result[0] - $result[1]) . '<br>';
+      echo "延長料金: " . $result[1] . '<br>';
+      echo "合計料金: " . $result[0] . '<br>';
+      echo "</div><br>";
     } else {
       echo "会場ID: " . $venueId . ' / ' . '料金体系' . $priceSystem . ' / ' . '入室: ' . $enter . ' / ' . '退室 :' . $leave . ' / ' . '期待する結果: ' . $expectValue . '<br>';
       echo "↓　システム算出結果<br><div style='background:black; color:red;font-weight:bold;'>";
