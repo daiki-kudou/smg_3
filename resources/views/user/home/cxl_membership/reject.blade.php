1f1403f2a82370e292b54568dc2cd8ac214f2ee2
maru-star <maruoka@web-trickster.com>
2021-10-20 19:45:32 +0900

    文言修正

--- resources/views/user/home/cxl_membership/reject.blade.php
+++ resources/views/user/home/cxl_membership/reject.blade.php
@@ -7,8 +7,8 @@
           <div class="attention-box">
             <p class="text-center"><i class="fas alert-icon fa-exclamation-triangle"></i></p>
             <div class="text-center mt-3">
-            <p class="section-ttl">利用が完了していない予約があります。</p>
-            <p class="text-left">
+            <!-- <p class="section-ttl">利用が完了していない予約があります。</p> -->
+            <p class="text-center">
                 退会をご希望の場合は、予約利用が完了してからお手続きをお願い致します。
             </p>
         </div>
