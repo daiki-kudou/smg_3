43a43fa4a10fc6123b77caa1131950c37141e69f
daiki-kudou <67718513+daiki-kudou@users.noreply.github.com>
2021-11-10 17:42:47 +0900

    管理者が仮抑え一覧よりチェックボックスを選択し削除 完了

--- app/Http/Controllers/Admin/PreReservationsController.php
+++ app/Http/Controllers/Admin/PreReservationsController.php
@@ -538,7 +538,7 @@ class PreReservationsController extends Controller
 
 
   /**
-   * Remove the specified resource from storage.
+   * Remove the specified resource from  storage.
    *
    * @param  int  $id
    * @return \Illuminate\Http\Response
