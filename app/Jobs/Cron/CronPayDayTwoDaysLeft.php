e5c0dff745ad5f466fe11f6d564eb57ef184ce34
daiki-kudou <67718513+daiki-kudou@users.noreply.github.com>
2021-11-19 12:09:03 +0900

    fix

---
+++ app/Jobs/Cron/CronPayDayTwoDaysLeft.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Jobs\Cron;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Mail\PayDayTwoDaysLeft;
+use App\Mail\FailedMail;
+use Mail;
+use Carbon\Carbon;
+
+
+class CronPayDayTwoDaysLeft implements ShouldQueue
+{
+  use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+  public $data;
+
+  /**
+   * Create a new job instance.
+   *
+   * @return void
+   */
+  public function __construct($data)
+  {
+    $this->data = $data;
+  }
+
+  /**
+   * Execute the job.
+   *
+   * @return void
+   */
+  public function handle()
+  {
+    // $admin = config('app.admin_email');
+    $subject = "【会議室お支払｜" . $this->data->category . "：" . $this->data->reservation_id . "】期日のお知らせ（SMG貸し会議室）";
+    Mail::to($this->data->user_email)
+      ->send(new PayDayTwoDaysLeft(
+        $this->data,
+        $subject
+      ));
+  }
+
+  /**
+   * 失敗したジョブの処理
+   *
+   * @param  Exception  $exception
+   * @return void
+   */
+  public function failed($exception)
+  {
+    $admin = config('app.admin_email');
+    $class_name = get_class($this);
+    $time = Carbon::now();
+    Mail::to($admin)
+      ->send(new FailedMail($exception, $class_name, $time));
+  }
+}
