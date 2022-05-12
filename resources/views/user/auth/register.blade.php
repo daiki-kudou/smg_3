7b93a9b926ff8ed1fe631eab956f7b5c2a09fc26
maru-star <maruoka@web-trickster.com>
2022-05-12 13:48:45 +0900

    smg-60完了

--- resources/views/user/auth/register.blade.php
+++ resources/views/user/auth/register.blade.php
@@ -296,7 +296,7 @@
       <tr>
         <th>パスワード確認<span class="txtRed">＊</span></th>
         <td>
-          {{ Form::password('password_confirmation', null, ['class' => 'text2']) }}
+          {{ Form::password('password_confirmation', ['class' => 'text2'], null) }}
           <p class="is-error-password_confirmation" style="color: red"></p>
           <p>※確認のため、もう一度パスワードを入力してください。</p>
         </td>
